7454295cde40573d6664f2d90cec3624e54a4a05
Anthony Popp <apopp168@gmail.com>
2017-09-21 14:59:18 -0700

    Documentation for Rectangle class

--- ConsoleApp1/Pong/Math/Rectangle.cs
+++ ConsoleApp1/Pong/Math/Rectangle.cs
@@ -38,47 +38,82 @@
 			CenterY();
 			return this;
 		}
+
+		/// <summary>
+		/// Returns the center position of this rectangle
+		/// </summary>
 		public Vector2 Center()
 		{
 			return new Vector2(Left() + HalfWidth(), Bottom() + HalfHeight());
 		}
+
+		/// <summary>
+		/// Centers this <see cref="Rectangle"/>'s x-axis on itself, returns a references for chaining
+		/// </summary>
 		public Rectangle CenterX()
 		{
 			position.x -= HalfWidth();
 			return this;
 		}
+
+		/// <summary>
+		/// Centers this <see cref="Rectangle"/>'s y-axis on itself, returns a reference for chaining
+		/// </summary>
 		public Rectangle CenterY()
 		{
 			position.y -= HalfHeight();
 			return this;
 		}
+
+		/// <summary>
+		/// The right of the <see cref="Rectangle"/> (x + width)
+		/// </summary>
 		public float Right()
 		{
 			return Left() + Width();
 		}
 
+		/// <summary>
+		/// The top of the <see cref="Rectangle"/> (y + height)
+		/// </summary>
 		public float Top()
 		{
 			return Bottom() + Height();
 		}
 
+		/// <summary>
+		/// The bottom of the <see cref="Rectangle"/> (y)
+		/// </summary>
 		public float Bottom()
 		{
 			return position.y;
 		}
+
+		/// <summary>
+		/// The left of the <see cref="Rectangle"/> (x)
+		/// </summary>
 		public float Left()
 		{
 			return position.x;
 		}
-
+		
+		/// <summary>
+		/// The width of this <see cref="Rectangle"/>
+		/// </summary>
+		/// <returns></returns>
 		public float Width()
 		{
 			return dimention.x;
 		}
+
+		/// <summary>
+		/// The height of this  <see cref="Rectangle"/>
+		/// </summary>
 		public float Height()
 		{
 			return dimention.y;
 		}
+
 		public float HalfWidth()
 		{
 			return Width() / 2.0f;
@@ -88,8 +123,16 @@
 			return Height() / 2.0f;
 		}
 
+		/// <summary>
+		/// Returns true if this <see cref="Rectangle"/> intersects with another one
+		/// </summary>
 		public bool Intersects(Rectangle other)
 		{
+			if (other == null)
+			{
+				return false;
+			}
+
 			if (Left() < other.Right() &&
 				Right() >= other.Left() &&
 				 Bottom() < other.Top() &&
